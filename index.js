42edb02456f4ec65a3c006f18290486406dc385a
Aaron Johnson <aaronjohnson@Pauls-MBP.lan>
2024-01-22 02:45:35 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keyup", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+const dodger = document.getElementById('dodger');
+
+function moveDodgerRight() {
+    const containerWidth = window.innerWidth;
+    let leftPosition = parseInt(dodger.style.left.replace('px', '')) || 0;
+    const maxWidth = containerWidth - dodger.offsetWidth;
+    if (leftPosition < maxWidth) {
+      dodger.style.left = leftPosition + 1 + 'px';
+    }
+  }
+
+document.addEventListener('keyup', function(event) {
+  if (event.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
